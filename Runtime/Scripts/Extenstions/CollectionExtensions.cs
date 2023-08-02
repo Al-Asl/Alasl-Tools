d2af2556da3255fa41e2ec685dc20d00e9c8d0bd
ahmad al-asl <ahmad.alasl@yahoo.com>
2023-08-03 01:31:00 +0300

    added ring buffer and sortedInsertion

--- Runtime/Scripts/Extenstions/CollectionExtensions.cs
+++ Runtime/Scripts/Extenstions/CollectionExtensions.cs
@@ -63,5 +63,38 @@ namespace AlaslTools
             for (int i = 0; i < count; i++)
                 list.Add(new T());
         }
+
+        public static void SortedInsertion<T>(this List<T> list, T value) where T : IComparable<T>
+        {
+            if (list.Count == 0 || value.CompareTo(list[list.Count - 1]) >= 0)
+            {
+                list.Add(value);
+                return;
+            }
+            if (value.CompareTo(list[0]) <= 0)
+            {
+                list.Insert(0, value);
+                return;
+            }
+
+            SortedInsertion(list, value, 0, list.Count);
+        }
+
+        private static void SortedInsertion<T>(List<T> list, T value, int start, int end) where T : IComparable<T>
+        {
+            int size = end - start;
+
+            int midIndex = start + size / 2;
+            int res = value.CompareTo(list[midIndex]);
+
+            if (res == 0)
+                list.Insert(midIndex, value);
+            else if (size == 1)
+                list.Insert(end, value);
+            else if (res > 0)
+                SortedInsertion(list, value, midIndex, end);
+            else
+                SortedInsertion(list, value, start, midIndex);
+        }
     }
 }
---
+++ Runtime/Scripts/RingBuffer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace AlaslTools
+{
+    public class RingBuffer<T>
+    {
+        private List<T> values;
+        private int start, end;
+        private int size;
+
+        public RingBuffer(int size)
+        {
+            values = new List<T>(size);
+            Clear();
+        }
+
+        public int Count => values.Count;
+        public bool Full => size == values.Count;
+
+        public void Clear()
+        {
+            values.Fill(values.Capacity, () => default(T));
+            start = 0; end = 0; size = 0;
+        }
+
+        public void Push(T item)
+        {
+            values[end] = item;
+            size = Mathf.Min(size + 1, values.Count);
+
+            Incremnet(ref end);
+            if (Full)
+                Incremnet(ref start);
+        }
+
+        public bool TryPop(out T item)
+        {
+            if (size > 0)
+            {
+                size = Mathf.Max(size - 1, 0);
+                Decrement(ref end);
+                item = values[end];
+                return true;
+            }
+            else
+            {
+                item = default;
+                return false;
+            }
+        }
+
+        private void Incremnet(ref int value)
+        {
+            value++;
+            if (value == values.Count)
+                value = 0;
+        }
+
+        private void Decrement(ref int value)
+        {
+            value--;
+            if (value == -1)
+                value = values.Count - 1;
+        }
+    }
+}
